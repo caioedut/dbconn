9a53c1bd4f1ea267f5eba19261d06d3c170c1a8b
Caio Teixeira <caio.teixeira@unyleya.com.br>
2023-11-22 19:55:52 -0300

    chore: persist settings

--- src/services/cookies.ts
+++ src/services/cookies.ts
@@ -4,6 +4,6 @@ export function getCookie(name: string) {
   return Cookies.get(name);
 }
 
-export function setCookie(name: string, value: string) {
-  return Cookies.set(name, value);
+export function setCookie(name: string, value: string, expires = 400) {
+  return Cookies.set(name, value, { expires });
 }
